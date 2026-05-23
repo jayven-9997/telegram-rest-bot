bc8cb8f189a440ca0d004a1c12294d168e93cb24
jayven-9997 <wenjie1411@gmail.com>
2026-05-24 00:14:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -796,7 +796,42 @@ bot.on('callback_query', async (query) => {
         }
       );
     }
+// ===== REMOVE =====
 
+if (
+  data.startsWith('remove_')
+) {
+
+  if (
+    query.from.id !== ADMIN_ID
+  ) return;
+
+  const parts =
+    data.split('_');
+
+  const stallId = parts[1];
+
+  const userId =
+    Number(parts[2]);
+
+  owners[stallId] =
+    owners[stallId].filter(
+      x => x !== userId
+    );
+
+  saveData();
+
+  return bot.editMessageText(
+    '✅已移除负责人',
+    {
+      chat_id:
+        query.from.id,
+
+      message_id:
+        query.message.message_id
+    }
+  );
+}
     // ===== CANCEL =====
 
     if (data === 'cancel') {
